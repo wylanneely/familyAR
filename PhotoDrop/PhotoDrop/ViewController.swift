24a964e4ffe3f56d453cf6a085419e3e770ed625
wylanneely <wylanneely@gmail.com>
2017-10-02 15:23:38 -0700

    Changing project to text with ar drop

--- PhotoDrop/PhotoDrop/ViewController.swift
+++ PhotoDrop/PhotoDrop/ViewController.swift
@@ -7,11 +7,132 @@
 //
 
 import UIKit
+import Vision
+import AVFoundation
 import SpriteKit
 import ARKit
 
 class ViewController: UIViewController, ARSKViewDelegate {
     
+    // TEXT STUFF
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLayoutSubviews() {
+        imageView.layer.sublayers?[0].frame = imageView.bounds
+    }
+    
+    
+    var session = AVCaptureSession()
+    var requests = [VNRequest]()
+    
+    func startTextDetection() {
+        let textRequest = VNDetectTextRectanglesRequest(completionHandler: self.detectTextHandler)
+        textRequest.reportCharacterBoxes = true
+        self.requests = [textRequest]
+    }
+    
+    func detectTextHandler(request: VNRequest, error: Error?) {
+        guard let observations = request.results else {
+            print("no result")
+            return
+        }
+        
+        let result = observations.map({$0 as? VNTextObservation})
+        
+        DispatchQueue.main.async() {
+            self.imageView.layer.sublayers?.removeSubrange(1...)
+            for region in result {
+                guard let rg = region else {
+                    continue
+                }
+                
+                self.highlightWord(box: rg)
+                
+                if let boxes = region?.characterBoxes {
+                    for characterBox in boxes {
+                        self.highlightLetters(box: characterBox)
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    func startLiveVideo() {
+        //1
+        session.sessionPreset = AVCaptureSession.Preset.photo
+        let captureDevice = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        //2
+        let deviceInput = try! AVCaptureDeviceInput(device: captureDevice!)
+        let deviceOutput = AVCaptureVideoDataOutput()
+        deviceOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_32BGRA)]
+        deviceOutput.setSampleBufferDelegate(self, queue: DispatchQueue.global(qos: DispatchQoS.QoSClass.default))
+        session.addInput(deviceInput)
+        session.addOutput(deviceOutput)
+        
+        //3
+        let imageLayer = AVCaptureVideoPreviewLayer(session: session)
+        imageLayer.frame = imageView.bounds
+        imageView.layer.addSublayer(imageLayer)
+        
+        session.startRunning()
+    }
+    
+    func highlightWord(box: VNTextObservation) {
+        guard let boxes = box.characterBoxes else {
+            return
+        }
+        
+        var maxX: CGFloat = 9999.0
+        var minX: CGFloat = 0.0
+        var maxY: CGFloat = 9999.0
+        var minY: CGFloat = 0.0
+        
+        for char in boxes {
+            if char.bottomLeft.x < maxX {
+                maxX = char.bottomLeft.x
+            }
+            if char.bottomRight.x > minX {
+                minX = char.bottomRight.x
+            }
+            if char.bottomRight.y < maxY {
+                maxY = char.bottomRight.y
+            }
+            if char.topRight.y > minY {
+                minY = char.topRight.y
+            }
+        }
+        
+        let xCord = maxX * imageView.frame.size.width
+        let yCord = (1 - minY) * imageView.frame.size.height
+        let width = (minX - maxX) * imageView.frame.size.width
+        let height = (minY - maxY) * imageView.frame.size.height
+        
+        let outline = CALayer()
+        outline.frame = CGRect(x: xCord, y: yCord, width: width, height: height)
+        outline.borderWidth = 2.0
+        outline.borderColor = UIColor.red.cgColor
+        
+        imageView.layer.addSublayer(outline)
+    }
+    
+    func highlightLetters(box: VNRectangleObservation) {
+        let xCord = box.topLeft.x * imageView.frame.size.width
+        let yCord = (1 - box.topLeft.y) * imageView.frame.size.height
+        let width = (box.topRight.x - box.bottomLeft.x) * imageView.frame.size.width
+        let height = (box.topLeft.y - box.bottomLeft.y) * imageView.frame.size.height
+        
+        let outline = CALayer()
+        outline.frame = CGRect(x: xCord, y: yCord, width: width, height: height)
+        outline.borderWidth = 1.0
+        outline.borderColor = UIColor.blue.cgColor
+        
+        imageView.layer.addSublayer(outline)
+    }
+    
+    
+    // AR STUFF
     @IBAction func lleytonButtonTapped(_ sender: Any) {
         self.arAssetName = "lleytonAR"
     }
@@ -39,7 +160,8 @@ class ViewController: UIViewController, ARSKViewDelegate {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
+        startLiveVideo()
+        startTextDetection()
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
 
@@ -66,21 +188,29 @@ class ViewController: UIViewController, ARSKViewDelegate {
             let node = SKSpriteNode(imageNamed: arAssetName)
             node.size = CGSize(width: 40.0, height: 40.0)
             return node
+    }
+}
 
-    }
+extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
     
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            return
+        }
         
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        var requestOptions:[VNImageOption : Any] = [:]
         
+        if let camData = CMGetAttachment(sampleBuffer, kCMSampleBufferAttachmentKey_CameraIntrinsicMatrix, nil) {
+            requestOptions = [.cameraIntrinsics:camData]
+        }
+        
+        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: CGImagePropertyOrientation(rawValue: 6)!, options: requestOptions)
+        
+        do {
+            try imageRequestHandler.perform(self.requests)
+        } catch {
+            print(error)
+        }
     }
 }
